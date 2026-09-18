4c620e33c8725e617e2bd7186a74088d15b846ad
TomTomThomas780 <thomasjinzihao@outlook.com>
2026-09-18 21:26:51 +0800

    weekly note

--- main.py
+++ main.py
@@ -1,2 +1,29 @@
 def define_env(env):
-    pass
+    def status_auth(status:str):
+        match status:
+            case "teacher":
+                return """
+!!! note
+
+    本文主要由教师撰写
+
+     
+"""
+            case "fromTeacher":
+                return """
+!!! note
+
+    本文由编者根据教师提供的资料撰写
+
+    
+"""
+            case "student":
+                return """
+
+!!! warning
+
+    本文主要由编者撰写
+
+    
+"""
+        
